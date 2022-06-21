1a69ee2a5e787457af5ea913abbbd0b4d29e9ce1
Oscarweb365 <neto.o@website365.ie>
2022-06-21 14:54:36 +0100

    updated useWeather hook

--- src/components/Pages/Weather.js
+++ src/components/Pages/Weather.js
@@ -1,33 +1,19 @@
-import { Fragment, useRef, useState } from "react";
+import { Fragment, useRef } from "react";
 import useWeather from "../../hooks/use-weather";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
-
 import ForecastTables from "../UI/ForecastTables";
 import WeatherTable from "../UI/WeatherTable";
 import { ReactComponent as Loading } from "../../assets/Loading.svg";
 import classes from "./Weather.module.css";
 
 const Weather = () => {
-  const [weather, setWeather] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const getWeather = useWeather();
-
   const selectedCity = useRef();
+  const { weather, isLoading, error, setCity } = useWeather();
+
   const City = require("country-state-city").City;
   const brazilianCities = City.getCitiesOfCountry("BR");
-
-  const weatherHandler = async () => {
-    setIsLoading(true);
-    const response = await getWeather(selectedCity.current.value);
-    if (typeof response == "string") {
-      setError(response);
-    } else {
-      setWeather(response);
-    }
-    setIsLoading(false);
-  };
+  const cityHandler = () => setCity(selectedCity.current.value);
   let content;
   if (weather) {
     content = (
@@ -53,7 +39,7 @@ const Weather = () => {
       <Navbar />
       <div className={classes.previsaoSection}>
         <h1>Escolha a Cidade para checar previsão do tempo</h1>
-        <select onChange={weatherHandler} ref={selectedCity}>
+        <select onChange={cityHandler} ref={selectedCity}>
           <option>Escolha a cidade:</option>
           {brazilianCities.map((city, index) => (
             <option key={index}>{city.name}</option>
--- src/hooks/use-weather.js
+++ src/hooks/use-weather.js
@@ -1,21 +1,34 @@
 import axios from "axios";
-const getWeather = async (cityName) => {
-  if (cityName === null || cityName === "") {
-    return;
-  }
-  try {
-    const weather = await axios.get("/weather", {
-      params: {
-        cityName: cityName,
-      },
-    });
-    return weather.data;
-  } catch (err) {
-    return err.message;
-  }
-};
+import { useEffect, useState } from "react";
 
-const useWeather = (cityName) => {
-  return getWeather;
-};
+function useWeather() {
+  const [city, setCity] = useState(null);
+  const [weather, setWeather] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (city === null) {
+      return;
+    }
+    setIsLoading(true);
+    axios
+      .get("/weather", {
+        params: {
+          cityName: city,
+        },
+      })
+      .then((response) => {
+        setWeather(response.data);
+      })
+      .catch((err) => {
+        setError(err.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [city]);
+  return { weather, isLoading, error, setCity };
+}
+
 export default useWeather;
